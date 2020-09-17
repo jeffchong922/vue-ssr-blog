91a2215ccb1ed29516ac3b59299c5fa9fa8ef43a
jeffchong <jeffchong922@outlook.com>
2020-09-17 22:52:58 +0800

    github bar 添加地址

--- src/components/GithubBar.vue
+++ src/components/GithubBar.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- github   -->
   <div class="fixed top-0 right-0 w-32 sm:w-48 transform rotate-45 translate-x-8 translate-y-5 sm:translate-x-10 sm:translate-y-10 text-center font-bold text-white bg-pink-900 border border-gray-600">
-    <a href="#">Github</a>
+    <a href="https://github.com/jeffchong922/vue-ssr-blog" target="_blank" rel="noopener">Github</a>
   </div>
   <!--  -->
 </template>
